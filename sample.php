bb8bbeb24a61ed4fbbf986df10220c0f8b5c8138
position3 <mickeylindberg@yahoo.co.jp>
2020-10-10 15:51:04 +0900

    配列修正

--- sample.php
+++ sample.php
@@ -5,7 +5,7 @@
     $result = omikuji();
   }
   function omikuji(){
-    $fortune = ["大吉" , "吉" , "中吉" , "小吉" ,"末吉" , "凶" , "大凶" ,"あきらめなさい"];
+    $fortune = ["大吉","吉","中吉","小吉","末吉","凶","大凶","あきらめなさい"];
     return $fortune[random_int(0,count($fortune)-1)];
   }
  ?>
@@ -20,7 +20,8 @@
 <body>
   <h1>おみくじ</h1>
   <p>おみくじの結果:<?php print htmlspecialchars($result,ENT_QUOTES,"UTF-8"); ?></p>
-  <form action="sample.php" method="POST"></form>
+  <form action="sample.php" method="POST">
   <button type ="submit" name="act" value="draw">おみくじをひく！</button>
+  </form>
 </body>
 </html>
