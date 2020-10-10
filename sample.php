2dcd843164121d7f0fbdfc0647108ed4a44e9b8c
position3 <mickeylindberg@yahoo.co.jp>
2020-10-10 15:24:00 +0900

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>おみくじ</title>
+</head>
+<body>
+  <h1>おみくじ</h1>
+  <p>おみくじの結果:無し</p>
+  <form action="sample.php" method="POST"></form>
+  <button type ="submit" name="act" value="draw">おみくじをひく！</button>
+</body>
+</html>
